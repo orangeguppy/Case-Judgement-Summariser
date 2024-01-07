68fe6370be50fe0530064bde37e08465e9b66e4b
orangeguppy <vuwet467@gmail.com>
2024-01-07 22:13:33 +0800

    Commenting

--- dataset/scrapers/sg_judgement_scraper.py
+++ dataset/scrapers/sg_judgement_scraper.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Scrape all pages
+# Scrape all pages (930), collect and combine all urls leading to individual judgement cases from all the pages
 def scrape_all_pages(num_pages):
     urls = []
     for i in range(1, num_pages + 1):
+        # ...CurrentPage={i}...: i represents the page number to scrape from
         url = "https://www.elitigation.sg/gd/Home/Index?Filter=SUPCT&YearOfDecision=All&SortBy=DateOfDecision&CurrentPage={i}&SortAscending=False&PageSize=0&Verbose=False&SearchQueryTime=0&SearchTotalHits=0&SearchMode=True&SpanMultiplePages=False"
         scraped_urls = extract_urls_from_page(url)
         urls.extend(scraped_urls)
     return urls
 
-# Get all links to judgement cases on a page
+# Get all links to judgement cases on a single page, this page contains a list of links linking to individual judgement cases
 def extract_urls_from_page(url):
     # Store results
     urls = []
@@ -20,14 +21,14 @@ def extract_urls_from_page(url):
     href_links = [link.get('href') for link in results]
     return href_links
 
-# Scrape the judgement (text form) from the page
+# Scrape the judgement (text form) from a single page (containing one judgement only)
 def scrape_judgement(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     judgement = soup.find("div", id="divJudgement").text
     return judgement
 
-# Scrape all judgements
+# Scrape all judgements from a set of urls
 def compile_judgements(urls):
     judgements = []
     for url in urls:
