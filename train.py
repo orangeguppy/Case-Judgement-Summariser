e88f06bef5a78d5658cfe5e0227540e294a60fa2
orangeguppy <vuwet467@gmail.com>
2024-02-04 17:43:35 +0800

    Use toy dataset

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ model1 = SummarizationModel(device, model_name="t5-small")
 print("Model created")
 
 # Declare country
-country = "India"
+country = "toy"
 
 # Declare filepaths for x and y (attrbutes and labels)
 judgement_folder = f"dataset/{country}/judgement"
