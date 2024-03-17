a014e108d86878377e765108ee219d15dac7dde4
unicesng <eunice.sng.2022@scis.smu.edu.sg>
2024-03-17 19:23:35 +0800

    Initialised website, created home page & models page

---
+++ website/lawlingo/src/app/page.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="relative place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/Lawlingo.svg"
+          alt="Next.js Logo"
+          width={300}
+          height={250}
+          priority
+        />
+      </div>
+      <div className="font-sans-serif  text-center">
+        <p>
+          This is a text summariser project meant to help lawyers, students, or anyone interested in law understand lengthy legal documents.
+        </p>
+        <p>
+          We built the text summariser by training 3 different transformers models - T5, Pegasus, GPT.
+        </p>
+      </div>
+      <div>
+        <Link href="/models">
+          <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+            Click here to get started
+          </button>
+        </Link>
+      </div>
+    </main>
+  );
+}
