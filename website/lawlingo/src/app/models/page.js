466da6e305b3192654c5280fdf38281a5a9ce7e8
unicesng <eunice.sng.2022@scis.smu.edu.sg>
2024-03-20 02:17:09 +0800

    Bug fix

--- website/lawlingo/src/app/models/page.js
+++ website/lawlingo/src/app/models/page.js
@@ -10,7 +10,7 @@ export default function Models() {
             <div className="flex">
                 {
                     models.map((model) =>
-                        <div href="#" class="block max-w-sm m-4 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                        <div key={model.name} href="#" class="block max-w-sm m-4 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{model.name}</h5>
                             <span class="font-normal text-gray-700 dark:text-gray-400">
